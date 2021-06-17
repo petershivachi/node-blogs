6ea565ce088ca21b3e075373e2c8ab6bbb01fb89
unknown <shitsukanepeter@gmail.com>
2021-06-17 13:04:05 +0300

    route files with express router

--- backend-assessment/server.js
+++ backend-assessment/server.js
@@ -7,6 +7,11 @@ app.get('/', (req, res) => res.send('API Running...'));
 //Connect Database
 connectDB();
 
+//Define Routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/blogs', require('./routes/api/blogs'));
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
